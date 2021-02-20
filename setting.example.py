e97931808fb12e3896d862e6e9f7248b2859c602
Mohamed Dief <mdaif1332@gmail.com>
2021-02-20 05:04:23 +0200

    Create setting.example.py

---
+++ setting.example.py
@@ -0,0 +1,18 @@
+DISCORD_TOKEN = "1234567890ABCD"
+USER = "/home/username"
+RECON_PATH = "/home/user/recon"
+ADMINS = ["Duck#6560"]
+DEBUG = False
+COMMANDS_PREFIX = "."
+ADMIN_CHANNEL = 812515205082906641
+
+TOOLS = {
+    "dirsearch":f"/{USER}/tools/dirsearch/",
+    "arjun":f"/{USER}/tools/Arjun/",
+    "gitgraber":f"/{USER}/tools/gitGraber/",
+    "findomain":f"/{USER}/tools/Finddomain/findomain-linux",
+    "nuclei-templates":f"/{USER}/nuclei-templates/",
+    "paramspider":f"{USER}/tools/ParamSpider/"
+}
+
+RCE = [';' , '`' , '$' , '(' , ')' , '|' , '&']
