cef56c593cdab9ef7333c46f8aadc11926c73795
Mohamed Dief <mdaif1332@gmail.com>
2021-02-20 05:02:44 +0200

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,18 @@
+DISCORD_TOKEN = ""
+USER = ""
+RECON_PATH = ""
+ADMINS = ['']
+DEBUG = True
+COMMANDS_PREFIX = "."
+ADMIN_CHANNEL = 
+
+TOOLS = {
+    "dirsearch":f"/{USER}/",
+    "arjun":f"/{USER}/",
+    "gitgraber":f"/{USER}/",
+    "findomain":f"/{USER}/",
+    "nuclei-templates":f"/{USER}/",
+    "paramspider":f"{USER}/"
+}
+
+RCE = [';' , '`' , '$' , '(' , ')' , '|' , '&']
