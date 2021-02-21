4ce88e5dd1217add21ee798fd3d98bbf3a8eddf8
Mohamed Dief <mdaif1332@gmail.com>
2021-02-21 07:09:56 +0200

    Create resolvedParser.py

---
+++ assets/resolvedParser.py
@@ -0,0 +1,33 @@
+from os import path
+
+# Define globals
+resolvedItems = {}
+
+def resolvedExists():
+    return path.exists('data/logs/resolved.easy')
+
+def resolvedParser():
+    global resolvedItems
+
+    if not resolvedExists():
+        open('data/logs/resolved.easy' , 'a').close()
+
+    resolvedContent = open('data/logs/resolved.easy', 'r').readlines()
+    if len(resolvedContent) == 1:
+        return False
+    else:
+        for singleLine in resolvedContent:
+            singleLine = singleLine.rstrip('\n')
+            items = singleLine.split('=')
+
+            websiteName = items[0]
+            fileName = items[1]
+
+            resolvedItems[websiteName] = fileName
+
+        return resolvedItems
+
+def resolvedWriter(Target , fileName):
+    with open('data/logs/resolved.easy' , 'a') as logsFile:
+        logsFile.write(f"{Target}={fileName}")
+        logsFile.close()
