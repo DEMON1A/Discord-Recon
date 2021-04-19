8892591007819fd413ef58457741f7bc2a2d364f
Mohamed Dief <mdaif1332@gmail.com>
2021-04-19 22:21:54 +0200

    Block arguments.

--- assets/CommandInjection.py
+++ assets/CommandInjection.py
@@ -1,5 +1,15 @@
+import re
+
 def commandInjection(argument , RCE):
     for char in argument:
         if char in RCE: return False
     
-    return True
+    argumentFinder = re.search(r"(..*\-|^\-)[a-zA-Z]", argument)
+    if argumentFinder != None:
+        argument = argument.split(' ')[1].strip()
+        if not argument.startswith('https://') or not argument.startswith('http://'):
+            return False
+        else:
+            return True
+    else:
+        return True
