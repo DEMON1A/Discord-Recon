f8fe10c6efb100fade87153f2ea16215f72617e8
Mohamed Dief <mdaif1332@gmail.com>
2021-08-12 23:40:00 +0200

    Add sanitize input

--- assets/CommandInjection.py
+++ assets/CommandInjection.py
@@ -1,11 +1,16 @@
 import re
+from shlex import quote
 
 def commandInjection(argument , RCE):
     for char in argument:
-        if char in RCE: return False
+        if char in RCE:
+            return False
     
     argumentFinder = re.search(r"(..*\-|^\-)[a-zA-Z]\s", argument)
     if argumentFinder != None:
         return False
     else:
         return True
+
+def sanitizeInput(argument):
+    return quote(argument)
