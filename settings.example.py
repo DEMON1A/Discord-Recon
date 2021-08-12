1621841b6772a98db3ad5f8e7141f64ef5152300
Mohamed Dief <mdaif1332@gmail.com>
2021-08-12 23:37:36 +0200

    Upgrading settings examples

--- settings.example.py
+++ settings.example.py
@@ -1,20 +1,27 @@
+from os import path
+
+BASE_PATH = path.abspath(path.join(__file__, "../"))
 DISCORD_TOKEN = "1234567890ABCD"
+SERVER_NAME = "My-Server"
+DEFAULT_DISCORD_WEBHOOK = "https://discord.com/api/webhooks/xyz"
 USER = "/home/username"
+ADMIN_ROLE = "Admins"
 RECON_PATH = "/home/user/recon"
-ADMINS = ["Duck#6560"]
 PYTHON_COMPILE = False
 DEBUG = False
-COMMANDS_PREFIX = "."
 ADMIN_CHANNEL = 812515205082906641
 DISABLE_NUCLEI_INFO = True
+NUCLEI_WEBHOOK = "https://discord.com/api/webhooks/xyz"
+COMMANDS_PREFIX = "."
 
 TOOLS = {
-    "dirsearch":"tools/dirsearch/",
-    "gitgraber":"tools/gitGraber/",
-    "findomain":"tools/findomain-linux",
-    "nuclei-templates":"tools/nuclei-templates/",
-    "paramspider":"tools/ParamSpider/",
-    "smuggler":"tools/smuggler/"
+    "dirsearch":f"/{USER}/tools/dirsearch/",
+    "arjun":f"/{USER}/tools/Arjun/",
+    "gitgraber":f"/{USER}/tools/gitGraber/",
+    "findomain":f"/{USER}/tools/Finddomain/findomain-linux",
+    "nuclei-templates":f"/{USER}/nuclei-templates/",
+    "paramspider":f"{USER}/tools/ParamSpider/",
+    "smuggler":f"{USER}/tools/smuggler/"
 }
 
-RCE = [';' , '`' , '$' , '(' , ')' , '|' , '&', '\n']
+RCE = [';' , '`' , '$' , '(' , ')' , '|' , '&' , '%', '\n', '<', '>']
