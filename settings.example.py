80ce6aa01d1c70b4875020aef7bf4c1bff29cc75
Mohamed Dief <mdaif1332@gmail.com>
2022-09-23 09:57:51 -0700

    Add parser

--- settings.example.py
+++ settings.example.py
@@ -1,4 +1,5 @@
 from os import path
+from urllib.parse import urlparse
 
 BASE_PATH = path.abspath(path.join(__file__, "../"))
 DISCORD_TOKEN = "1234567890ABCD"
@@ -8,18 +9,19 @@ ADMIN_ROLE = "Admins"
 RECON_PATH = "/home/user/recon"
 DEBUG = False
 ADMIN_CHANNEL = 812515205082906641
+ADMIN_CHANNEL = int(urlparse(str(ADMIN_CHANNEL)).path.split('/')[-1:][0])
 DISABLE_NUCLEI_INFO = True
 NUCLEI_WEBHOOK = "https://discord.com/api/webhooks/xyz"
 COMMANDS_PREFIX = "."
 
 TOOLS = {
-    "dirsearch":f"/{USER}/tools/dirsearch/",
-    "arjun":f"/{USER}/tools/Arjun/",
-    "gitgraber":f"/{USER}/tools/gitGraber/",
-    "findomain":f"/{USER}/tools/Finddomain/findomain-linux",
-    "nuclei-templates":f"/{USER}/nuclei-templates/",
-    "paramspider":f"{USER}/tools/ParamSpider/",
-    "smuggler":f"{USER}/tools/smuggler/"
+    "dirsearch":f"{BASE_PATH}/tools/dirsearch/",
+    "arjun":f"{BASE_PATH}/tools/Arjun/",
+    "gitgraber":f"{BASE_PATH}/tools/gitGraber/",
+    "findomain":f"{BASE_PATH}/tools/Finddomain/findomain-linux",
+    "nuclei-templates":f"{BASE_PATH}/nuclei-templates/",
+    "paramspider":f"{BASE_PATH}/tools/ParamSpider/",
+    "smuggler":f"{BASE_PATH}/tools/smuggler/"
 }
 
 RCE = [';' , '`' , '$' , '(' , ')' , '|' , '&' , '%', '\n', '<', '>']
