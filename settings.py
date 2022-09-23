e71c9452cdb58962a47d0937a23200364a647773
Mohamed Dief <mdaif1332@gmail.com>
2022-09-23 10:02:38 -0700

    Add parser

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from os import path
+from urllib.parse import urlparse
 
 BASE_PATH = path.abspath(path.join(__file__, "../"))
 DISCORD_TOKEN = ""
@@ -8,17 +9,18 @@ ADMIN_ROLE = ""
 RECON_PATH = ""
 DEBUG = True
 ADMIN_CHANNEL = 812515205082906644
+ADMIN_CHANNEL = int(urlparse(str(ADMIN_CHANNEL)).path.split('/')[-1:][0])
 DISABLE_NUCLEI_INFO = True
 NUCLEI_WEBHOOK = ""
 COMMANDS_PREFIX = "."
 
 TOOLS = {
-    "dirsearch":"tools/dirsearch/",
-    "gitgraber":"tools/gitGraber/",
-    "findomain":"tools/findomain-linux",
-    "nuclei-templates":"tools/nuclei-templates/",
-    "paramspider":"tools/ParamSpider/",
-    "smuggler":"tools/smuggler/"
+    "dirsearch": f"{BASE_PATH}/tools/dirsearch/",
+    "gitgraber": f"{BASE_PATH}tools/gitGraber/",
+    "findomain": f"{BASE_PATH}/tools/findomain-linux",
+    "nuclei-templates": f"{BASE_PATH}/tools/nuclei-templates/",
+    "paramspider": f"{BASE_PATH}/tools/ParamSpider/",
+    "smuggler": f"{BASE_PATH}/tools/smuggler/"
 }
 
 RCE = [';' , '`' , '$' , '(' , ')' , '|' , '&' , '%', '\n', '<', '>']
